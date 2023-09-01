73db1fcb53b258d1bd342fd384b07e87f21e5842
yeasin2448 <ayeasin108@gmail.com>
2023-09-01 12:43:35 +0600

    frist update

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-course';
+
+
+  public names! : any [];
+
+  addNamesOnClick(){
+
+    this.names.push("test");
+
+  }
+
+
+}
